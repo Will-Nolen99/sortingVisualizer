b108a0807f5ec9d24a781d1d75a5ef3c99c7d4f6
William Nolen <wpncomps@yahoo.com>
2020-10-17 01:26:52 -0400

    File organization

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # sortingVisualizer
 Visualization of sorting algorithms
+
+
+
+
+
+
+
+
+
+   colors                Hex                 rgb
+   ######               ######              ######
+
+Raisin Black            171A21            (23, 26, 33)
+Cadet                   617073            (97, 112, 115)
+Periwinkle Crayola      C3C9E9            (195, 201, 233)
+Sea Green Crayola       44FFD1            (68, 255, 209)
+Medium Spring Green     40F99B            (64, 249, 155)
+Brink Pink              E85D75            (232, 93, 117)
+
+
+
--- main.py
+++ main.py
@@ -4,20 +4,6 @@ import menu
 from element import make_array
 from draw import draw_menu
 
-'''
-
-   colors                Hex                 rgb
-   ######               ######              ######
-
-Raisin Black            171A21            (23, 26, 33)
-Cadet                   617073            (97, 112, 115)
-Periwinkle Crayola      C3C9E9            (195, 201, 233)
-Sea Green Crayola       44FFD1            (68, 255, 209)
-Medium Spring Green     40F99B            (64, 249, 155)
-Brink Pink              E85D75            (232, 93, 117)
-
-'''
-
 
 def main():
     
